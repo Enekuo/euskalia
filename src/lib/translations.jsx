e0b8918aeda8f48aa8a637a74743a68de9e82ebd
Eneko Aramberri <aramberrieneko@gmail.com>
2026-02-06 17:38:39 +0100

    17

--- src/lib/translations.jsx
+++ src/lib/translations.jsx
@@ -149,7 +149,7 @@ translator_limit_reached: {
     // Documentos
     choose_file_title: { ES: "Elige tu archivo o carpeta", EUS: "Aukeratu zure fitxategia edo karpeta", EN: "Choose your file or folder", FR: "Choisis ton fichier ou dossier" },
     accepted_formats:  { ES: "Formatos admitidos: .pdf, .docx, .txt", EUS: "Onartutako formatuak: .pdf, .docx, .txt", EN: "Allowed formats: .pdf, .docx, .txt", FR: "Formats autorisés : .pdf, .docx, .txt"},
-    folder_hint:       { ES: "Puedes arrastrar varios archivos.", EUS: "Fitxategi bat baino gehiago arrasta ditzakezu.", EN: "You can drag multiple files.", FR: "Tu peux glisser plusieurs fichiers." },
+    folder_hint:       { ES: "Se permite un solo archivo por resultado.", EUS: "Emaitza bakoitzeko fitxategi bakarra onartzen da.", EN: "Only one file is allowed per result.", FR: "Un seul fichier est autorisé par résultat."},
     remove:            { ES: "Quitar", EUS: "Kendu", EN: "Remove", FR: "Retirer" },
 
     // URLs
