75ac265cd8c0d30c4ce41f9a9644a53f3d22f941
Eneko Aramberri <aramberrieneko@gmail.com>
2026-03-16 18:16:11 +0100

    48

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -99,6 +99,6 @@ export default async function handler(req, res) {
     return res.status(500).json({
       error: "Server error",
       message: String(err?.message || err),
-    });
+    }); 
   }
 }
