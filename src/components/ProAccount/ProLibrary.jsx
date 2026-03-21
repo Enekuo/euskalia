de52a76782e6cb873642448f92ca8e3eefe8dde8
Eneko Aramberri <aramberrieneko@gmail.com>
2026-03-21 22:54:21 +0100

    86

--- src/components/ProAccount/ProLibrary.jsx
+++ src/components/ProAccount/ProLibrary.jsx
@@ -526,7 +526,7 @@ export default function ProLibrary() {
                             </div>
 
                             <h3
-                              className="mt-1 text-[18px] leading-[24px] pr-4"
+                              className="mt-2 text-[18px] leading-[24px] pr-4"
                               style={{
                                 display: "-webkit-box",
                                 WebkitLineClamp: 2,
